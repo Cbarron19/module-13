ebc00b0f9474675f3b01c042bbdb53a573d1d014
Cbarron19 <34284162+Cbarron19@users.noreply.github.com>
2018-04-06 07:56:16 -0500

    Update 13.2.1.py

--- 13.2.1.py
+++ 13.2.1.py
@@ -1,8 +1,6 @@
 import RoboPiLib as RPL
-from setup import RPL
-import time
-import post_to_web as PTW
 RPL.RoboPiInit("/dev/ttyAMA0",115200)
+import time
 
 motorL = 1
 motorR = 7
@@ -10,18 +8,14 @@ sensor_pin = 2
 i = 4.9
 
 while RPL.digitalRead(sensor_pin) == 1:
-    PTW.state['d1'] = RPL.digitalRead(sensor_pin)
     RPL.servoWrite(motorR, 1000)
     RPL.servoWrite(motorL, 2000)
-    PTW.post()
     time.sleep(0.3)
     if RPL.digitalRead(sensor_pin) != 1:
         break
 
 while RPL.digitalRead(sensor_pin) == 0:
-    PTW.state['d1'] = RPL.digitalRead(sensor_pin)
     move = time.time()
-    PTW.post()
     while time.time() < (move + i):
         RPL.servoWrite(motorR, 1475)
         RPL.servoWrite(motorL, 1520)
