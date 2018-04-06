bd5a440e3e7dd493cf68ea4b6d4c5d6cfe3bb7f0
Cbarron19 <34284162+Cbarron19@users.noreply.github.com>
2018-04-06 10:13:19 -0500

    Update 13.2.1.py

--- 13.2.1.py
+++ 13.2.1.py
@@ -2,10 +2,10 @@ import RoboPiLib as RPL
 RPL.RoboPiInit("/dev/ttyAMA0",115200)
 import time
 
-motorL = 1
-motorR = 7
+motorL = 1 # Left motor pin
+motorR = 7 # Right motor pin
 sensor_pin = 2
-i = 4.9
+i = 4.9 # the rate of time
 
 while RPL.digitalRead(sensor_pin) == 1:
     RPL.servoWrite(motorR, 1000)
@@ -13,12 +13,13 @@ while RPL.digitalRead(sensor_pin) == 1:
     time.sleep(0.3)
     if RPL.digitalRead(sensor_pin) != 1:
         break
+# pauses then ends the while loop
 
 while RPL.digitalRead(sensor_pin) == 0:
     move = time.time()
-    while time.time() < (move + i):
+    while time.time() < (move + i):  # While the time is less than move + i, it will continue to move forward at a slower speed
         RPL.servoWrite(motorR, 1475)
         RPL.servoWrite(motorL, 1520)
-    while time.time() > (move + i):
+    while time.time() > (move + i): # While the time is greater than move + i, it will stop
         RPL.servoWrite(motorR, 0)
         RPL.servoWrite(motorL, 0)
